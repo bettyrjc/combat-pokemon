a0714bc5681f7bf721a4665ddfa0a593b2bdfa61
Betty Jimenez <bettyjimenez3010@gmail.com>
2024-10-25 15:27:01 -0300

    :lipstick: add: components bases

---
+++ src/modules/shared/buttons/CircleButton.tsx
@@ -0,0 +1,27 @@
+
+const CircleButton = ({
+  onClick,
+  icon,
+  isDisabled,
+  isLoading,
+}: {
+  onClick?: () => void
+  icon?: React.ReactNode
+  className?: string
+  isDisabled?: boolean
+  isLoading?: boolean
+}) => {
+  return (
+    <button className={`
+      p-1  rounded-full shadow-xl transition-all duration-300
+      ${isDisabled ? 'bg-gray-300 cursor-none' : 'bg-primary cursor-pointer hover:bg-orange-600'}
+      `}
+      onClick={onClick}
+      disabled={isDisabled || isLoading}
+    >
+      {icon}
+    </button>
+  )
+}
+
+export default CircleButton
